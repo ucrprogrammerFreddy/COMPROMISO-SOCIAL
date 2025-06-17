72066ba31da865e5726555aa162d87fae0b85819
Danier Jordany Morales Castro <danier.morales@ucr.ac.cr>
2025-06-17 11:56:12 -0600

    Modelos
    Modelos usuario, visita, visitante

---
+++ EduAcceso/modelo/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduAcceso.Modelo
+{
+    public class Usuario
+    {
+
+        public int idUsuario { get; set; }
+        public string nombre { get; set; }
+        public string correo { get; set; }
+        public int telefono { get; set; }
+        public string clave { get; set; }
+        public string rol { get; set; }
+
+    }
+}
---
+++ EduAcceso/modelo/Visitante.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduAcceso.Modelo
+{
+    public class Visitante
+    {
+        public int idVisitante {  get; set; }
+        public string cedula { get; set; }
+        public string nombre { get; set; }
+        public string correo { get; set; }
+        public int telefono { get; set; }
+        public int direccion {  get; set; }
+    }
+}
